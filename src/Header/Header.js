170bb144148e2837b5d2c7d26686ba7f48985ea8
nickdepaolo <79952881+nickdepaolo@users.noreply.github.com>
2022-05-09 00:37:56 -0400

    0509 0037

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,10 +1,24 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react';
+import Hamburger from 'hamburger-react'
+
 
 const Header = () => {
+    const [toggle, setToggle] = useState(false)
 
     return(
         <div id='header'>
             <h1>Header</h1>
+            <Hamburger onToggle={toggled => {
+                if (toggled){
+                    {setToggle(true)}         
+                }else{
+                    {setToggle(false)}
+                }
+            }}/>
+            {toggle && 
+                <div id='toggled'>
+                    <h1>Menu</h1>
+                </div>}
         </div>
     )
 
